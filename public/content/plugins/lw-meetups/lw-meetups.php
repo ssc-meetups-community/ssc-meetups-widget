7449507818cb559276545a98bb4ede76dbe91e90
Taymon A. Beal <taymonbeal@gmail.com>
2018-05-27 20:55:31 -0400

    Add hello-world widget

---
+++ public/content/plugins/lw-meetups/lw-meetups.php
@@ -0,0 +1,23 @@
+class LW_Meetups_Widget extends WP_Widget {
+
+	public function __construct() {
+		parent::__construct(
+			'lw_meetups',
+			'Upcoming Meetups',
+			array( 'description' => 'Lists upcoming meetups from LessWrong' )
+		);
+	}
+
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', 'Upcoming Meetups' ) . $args['after_title'];
+		}
+		echo esc_html__( 'Hello, World!', 'text_domain' );
+		echo $args['after_widget'];
+	}
+}
+
+add_action( 'widgets_init', function() {
+	register_widget( 'LW_Meetups_Widget' );
+});
